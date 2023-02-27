e17cbbb4b37c878294f8a2b5c59366504b03f4ef
mikafeng <mikafengler@gmail.com>
2023-02-27 16:58:01 -0500

    add buttons in html and variables in script

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
 //clickevent for start button
 //timer doesn't start until clickevent
-var startQuiz = document.getElementById("startQuiz");
+var quizButton = document.getElementById("startQuiz");
 var timeEl = document.getElementById("countDown");
+var quizContainer = document.getElementById("quiz");
+var showResults = document.getElementById("results");
 var timeLeft = 30;
 
-
-startQuiz.addEventListener("click", function(){
 //make 30 secs in min.sec format and change to start time 3min00sec
 function countDown(){
+
     var timerInterval = setInterval(function(){
         timeLeft--;
         timeEl.textContent = timeLeft + " left!";
@@ -17,11 +18,11 @@ function countDown(){
         displayMessage();
         }   
 }, 1000)};
-countDown();
-});
+
 
 //tried to add countDown function w/in button click for startQuiz
 //works but only one click...not sure if this is best way.
 
 //need function displayMessage for going to page of entering highscore
 //located in activities 9 and 10
+quizButton.addEventListener("click", countDown());
