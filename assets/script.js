76e621db561f7c0de38c1ef58580b9eed372079d
mikafeng <mikafengler@gmail.com>
2023-02-27 16:08:26 -0500

    added start button to begin countdown

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
+//clickevent for start button
+//timer doesn't start until clickevent
+var startQuiz = document.getElementById("startQuiz");
 var timeEl = document.getElementById("countDown");
 var timeLeft = 30;
 
+
+startQuiz.addEventListener("click", function(){
+//make 30 secs in min.sec format and change to start time 3min00sec
 function countDown(){
     var timerInterval = setInterval(function(){
         timeLeft--;
@@ -11,5 +17,11 @@ function countDown(){
         displayMessage();
         }   
 }, 1000)};
-
 countDown();
+});
+
+//tried to add countDown function w/in button click for startQuiz
+//works but only one click...not sure if this is best way.
+
+//need function displayMessage for going to page of entering highscore
+//located in activities 9 and 10
