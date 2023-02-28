e65cf23e57692d6fa702d583886777af3456fd90
mikafeng <mikafengler@gmail.com>
2023-02-28 00:39:44 -0500

    added more getElBId vars, added startQuiz function, added holder questions

--- assets/script.js
+++ assets/script.js
@@ -2,32 +2,96 @@
 //timer doesn't start until clickevent
 //array of questions
 
-var quizButton = document.getElementById("startQuiz");
-var timeEl = document.getElementById("countDown");
+//TIMER COUNTDOWN
+var startBtn = document.getElementById("startBtn");
 var quizContainer = document.getElementById("quiz");
+var timeEl = document.getElementById("countDown");
 var showResults = document.getElementById("results");
+var progressEl = document.getElementById("progress");
+var questionEl = document.getElementById("question");
+var choicesEl = document.getElementById("option");
 
-var timeLeft = 30;
+var timeLeft = 60;
 
-var questions = []
 
-//make 30 secs in min.sec format and change to start time 3min00sec
+//Question Variables
+var questionCount = 0;
+var score = 0;
+
+
+startBtn.addEventListener("click", startQuiz)
+
+function startQuiz () {
+    startBtn.classList.add('hidden');
+    progressEl.classList.remove('hidden');
+    questionEl.classList.remove('hidden');
+    choicesEl.classList.remove('hidden');
+    questionCount = 0;
+    score = 0;
+    questions = [...questions];
+    console.log(questions);
+    countDown();
+};
+
 function countDown(){
 
     var timerInterval = setInterval(function(){
         timeLeft--;
-        timeEl.textContent = timeLeft + " left!";
+        timeEl.textContent = timeLeft + " seconds left!";
     
     if(timeLeft === 0) {
         clearInterval(timerInterval);
-        displayMessage();
+        displayMessage("time out");
         }   
 }, 1000)};
 
 
-//tried to add countDown function w/in button click for startQuiz
-//works but only one click...not sure if this is best way.
 
-//need function displayMessage for going to page of entering highscore
-//located in activities 9 and 10
-quizButton.addEventListener("click", countDown());
+// function guess(id, guess) {
+//     let button = document.getElementById(id);
+//     button.onclick = function () {
+//         quiz.guess(guess);
+//         displayQuestion();
+//     }
+// };
+
+// show progress
+// function showProgress() {
+//     let currentQuestionNumber = quiz.questionIndex + 1;
+//     let progressEl = document.getElementById("progress");
+//     progressEl.innerHTML = `Question ${currentQuestionNumber} of ${quiz.question.length}`;
+// };
+
+//show score
+
+//create quiz questions
+let questions = [
+    {
+        "question": "Inside which HTML element do we put the JavaScript??",
+        "choice1": "<script>",
+        "choice2": "<javascript>",
+        "choice3": "<js>",
+        "answer": 1
+    },
+    {
+        "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        "choice1": "<script href='xxx.js'>",
+        "choice2": "<script name='xxx.js'>",
+        "choice3": "<script src='xxx.js'>",
+        "answer": 3
+    },
+    {
+        "question": " How do you write 'Hello World' in an alert box?",
+        "choice1": "msgBox('Hello World');",
+        "choice2": "alertBox('Hello World');",
+        "choice3": "alert('Hello World');",
+        "answer": 3
+    }
+]
+
+// let quiz = new Quiz(questions);
+
+//display questions
+// displayQuestion();
+
+
