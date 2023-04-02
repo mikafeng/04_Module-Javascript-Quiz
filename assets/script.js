f9f4cfd971b067abff448b2940d8ce7db26c4477
mikafeng <mikafengler@gmail.com>
2023-04-02 18:16:03 -0400

    update readme

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ var quizContainer = document.getElementById("quiz");
 var timeEl = document.getElementById("countDown");
 var results = document.getElementById("results");
 var progressEl = document.getElementById("progress");
-var questionEl = document.getElementById("question");
+var questionEl = document.getElementById("questions");
 // var choicesEl = document.getElementById("option");
 var answersEl = document.getElementById("answers")
 var currentQIndex;
@@ -45,24 +45,56 @@ function startQuiz () {
     questionCount = 0;
     score = 0;
     currentQIndex = 0;
-    renderQs();
     // questions = [...questions];
+    renderQs();
     // console.log(questions);
     countDown();
 };
 
+
 //FUNCTION render question
-function renderQs() {
-    showQ(questions[currentQIndex]);
-    // for(let i=0; i<questions.length; i++) {
-    //     console.log(questions[i]);
+// function renderQs() {
+//     showQ(questions[currentQIndex]);
+    // for (i=0; i<answersEl.children.length; i++){
+    //     console.log(answers.children[i]);
     // }
-};
 
-function showQ(question) {
-    questionEl.innerText = question.question
-};
+// };
 
+// function showQ(question) {
+//     questionEl.innerText = question.question
+//     question.answers.forEach(answer => {
+//         var button = document.createElement('button')
+//         button.innerText = answer.text;
+//         button.classList.add('btn')
+//         if (answer.correct) {
+//             button.dataset.correct = answer.correct
+//         }
+//         button.addEventListener('click', selectAnswer)
+//         answersEl.appendChild(button)
+//     })
+// };
+
+function selectAnswer(e) {
+//     const selectedButton = e.target
+//     const correct = selectedButton.dataset.correct
+//     setStatusClass(document.body, correct)
+//     Array.from(answerButtonsElement.children).forEach(button => {
+//         setStatusClass(button, button.dataset.correct)
+//     })
+//     if (shuffledQuestions.length > currentQuestionIndex + 1) {
+//         nextButton.classList.remove('hide')
+//         console.log('this button got clicked')
+//     } else {
+//         startButton.innerText = 'Restart'
+//         startButton.classList.remove('hide')
+//         console.log('what?')
+//     }
+}
+
+// function showChoices(answers){
+//     questionEl.innerText = question.choices;
+// }
 //FUNCTION timer starts
 function countDown(){
 
@@ -78,6 +110,12 @@ function countDown(){
 
 
 
+function renderQs() {
+    questions.textContent = questions[currentQIndex].question;
+    for (i = 0; i < answersEl.children.length; i++) {
+        answersEl.children[i].children[0].textContent = `${(i + 1)}: ${questions[currentQIndex].choices[i]}`;
+    }
+}
 // renderQs();
 
 // function guess(id, guess) {
@@ -98,22 +136,38 @@ function countDown(){
 //show score
 
 //create quiz questions
+
+function evaluate(){
+    if (correct === true) {
+        button.style
+    }
+}
 //QUESTIONS array
 const questions = [
     {
-        question: "Inside which HTML element do we put the JavaScript??",
-        choices: ["<script>", "<javascript>", "<js>"],
-        answer: 1
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            { text: "<script>", correct: true },
+            { text: "<javascript>", correct: false },
+            { text: "<js>", correct: false }
+        ],
     },
     {
-        "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        "choices": ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>"],
-        "answer": 2
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        "answers": [
+            { text: "<script href='xxx.js'>", correct: false},
+            { text: "<script name='xxx.js'>", correct: true},
+            { text: "<script src='xxx.js'>", correct: false}
+        ],
+    
     },
     {
-        "question": " How do you write 'Hello World' in an alert box?",
-        "choices": ["msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
-        "answer": 0
+        question: " How do you write 'Hello World' in an alert box?",
+        "answers": [
+            { tet: "msgBox('Hello World');", correct: true },
+            { text :"alertBox('Hello World');", correct: false}, 
+            { text :"alert('Hello World');", correct: false}
+        ],
     }
 ];
 
